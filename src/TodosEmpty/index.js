8f104314cd09a0223b90b1fd090b1583c6733d88
Diiegor98 <diiegor98@gmail.com>
2024-02-17 13:23:18 -0300

    bugs 2

--- src/TodosEmpty/index.js
+++ src/TodosEmpty/index.js
@@ -1,7 +1,20 @@
+import { IconContext } from "react-icons";
+import { IoCreateOutline } from "react-icons/io5";
+import './TodosEmpty.css'
+
 const TodosEmpty = () => {
-    return(
-        <p>Todas</p>
-    )
-}
+  return (
+    <div className="empty-container">
+      <IconContext.Provider
+        value={{
+          className: `empty-icon`,
+        }}
+      >
+        <IoCreateOutline />
+        <p className="empty-text">Crea una nueva para comenzar</p>
+      </IconContext.Provider>
+    </div>
+  );
+};
 
 export default TodosEmpty;
